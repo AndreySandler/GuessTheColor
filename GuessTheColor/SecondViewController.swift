5f141ccf9aeeeab5969842c19edb2cfe5c3fe1b2
AndreySandler <mrdreem13@gmail.com>
2022-09-21 14:50:03 +0300

    Setup secondViewController

---
+++ GuessTheColor/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  GuessTheColor
+//
+//  Created by Andrey Sandler on 21.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var redButton: UIButton!
+    @IBOutlet var yellowButton: UIButton!
+    @IBOutlet var greenButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func redButtonDidTapped() {
+    }
+    
+    @IBAction func yellowButtonDidTapped() {
+    }
+    
+    @IBAction func greenButtonDidTapped() {
+    }
+}
--- GuessTheColor/ViewController.swift
+++ GuessTheColor/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var greetingLabel: UILabel!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.layer.masksToBounds = true
+        greetingLabel.layer.cornerRadius = 30
+        
+        
+        
     }
 
 
